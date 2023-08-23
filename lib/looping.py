2913abd552ead042f8f8fde837d05d1b4b6ac155
Your Name <youremail@example.com>
2023-08-23 17:14:30 +0300

    commit message

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,27 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+ counter = 10
+ while counter > 0:
+   print(counter)
+   counter -= 1
+   print("Happy New Year!")
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
 
-def fizzbuzz():
+def fizzbuzz(num):
     # code goes here!
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+       return  "FizzBuzz"
+    elif num % 3 == 0:
+       return "Fizz"
+    elif num % 5 == 0:
+       return "Buzz"
+    else:
+       return num
+
+def fizzbuzzPrinter(num):
+   for num in range (1, 101):
+    print(fizzbuzz(num))
